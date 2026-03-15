7835119793ed20773e2a1a33530cb20bd4db8a33
visithra-vijayarethinam <visithra.vijayarethinam-famteam@outlook.com>
2026-03-15 18:06:50 +0530

    intial commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Nav from './nav.jsx'
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+ <>
+    <Nav />
+   <div class="main">
+    <h1>This is React js </h1>
+   </div>
+   </> 
+   
+  )
+}
+
+export default App
